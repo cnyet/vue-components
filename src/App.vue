2f878250b093cde9800b4afad0477ecc55c33fd6
Yate Hong <yateup@gmail.com>
2019-02-18 22:07:02 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <transition name="fade">
+      <keep-alive>
+        <router-view />
+      </keep-alive>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+};
+</script>
+
+<style lang="scss" src="./assets/styles/main.scss"></style>
---
+++ src/views/DemosHome.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="demosHome-container">
+    <h1>首页</h1>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss">
+.demosHome-container{
+
+}
+</style>
