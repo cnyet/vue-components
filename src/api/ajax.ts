86e97a08ac07ebcf6230cad0dd7c43a2a5fea71f
yates <yateup@gmail.com>
2020-06-29 21:44:08 +0800

    feat: 🎸 add environment configuration

---
+++ src/api/ajax.ts
@@ -0,0 +1,148 @@
+/**
+ * @author: yates
+ * @description: 封装HTTP请求
+ */
+import axios from 'axios';
+axios.defaults.baseURL = process.env.API_URL;
+
+export interface ResponseData {
+  code: number;
+  data: any | any[];
+  message: string;
+}
+
+// 添加请求时拦截器
+axios.interceptors.request.use((config) => {
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
+
+// 添加响应时的拦截器
+axios.interceptors.response.use((response) => {
+  return response.data;
+}, (error) => {
+  return Promise.reject(error);
+});
+
+/**
+ * GET 请求
+ * @Author   Yates
+ * @DateTime 2020-06-29
+ * @param    {[type]}   url    请求的 url
+ * @param    {[type]}   params 请求传递的参数
+ * @param    {[type]}   config 请求额外的配置信息
+ */
+function GET(url, params, config) {
+  return new Promise((resolve, reject) => {
+    axios.get(url, params, config).then((res) => {
+      resolve(res.data);
+    }).catch((err) => {
+      reject(err);
+    });
+  });
+}
+
+/**
+ * POST 请求
+ * @Author   Yates
+ * @DateTime 2020-06-29
+ * @param    {[type]}   url    请求的 url
+ * @param    {[type]}   data   请求传递的参数
+ * @param    {[type]}   config 请求额外的配置信息
+ */
+function POST(url, data, config) {
+  return new Promise((resolve, reject) => {
+    const defaultConfig = {
+      transformRequest: [
+        function(requestData, headers) {
+          headers['Content-Type'] = 'application/json';
+          return JSON.stringify(requestData);
+        }
+      ]
+    };
+    axios.post(url, data, (config || defaultConfig)).then((res) => {
+      resolve(res.data);
+    }).catch((err) => {
+      reject(err);
+    });
+  });
+}
+
+/**
+ * PUT 请求
+ * @Author   Yates
+ * @DateTime 2020-06-29
+ * @param    {[type]}   url    请求的 url
+ * @param    {[type]}   data   请求传递的参数
+ * @param    {[type]}   config 请求额外的配置信息
+ */
+function PUT(url, data, config) {
+  return new Promise((resolve, reject) => {
+    const defaultConfig = {
+      transformRequest: [
+        function(requestData, headers) {
+          headers['Content-Type'] = 'application/json';
+          return JSON.stringify(requestData);
+        }
+      ]
+    };
+    axios.put(url, data, (config || defaultConfig)).then((res) => {
+      resolve(res.data);
+    }).catch((err) => {
+      reject(err);
+    });
+  });
+}
+
+/**
+ * PATCH 请求
+ * @Author   Yates
+ * @DateTime 2020-06-29
+ * @param    {[type]}   url    请求的 url
+ * @param    {[type]}   data   请求传递的参数
+ * @param    {[type]}   config 请求额外的配置信息
+ */
+function PATCH(url, data, config) {
+  return new Promise((resolve, reject) => {
+    const defaultConfig = {
+      transformRequest: [
+        function(requestData, headers) {
+          headers['Content-Type'] = 'application/json';
+          return JSON.stringify(requestData);
+        }
+      ]
+    };
+    axios.patch(url, data, (config || defaultConfig)).then((res) => {
+      resolve(res.data);
+    }).catch((err) => {
+      reject(err);
+    });
+  });
+}
+
+/**
+ * DELETE 请求
+ * @Author   Yates
+ * @DateTime 2020-06-29
+ * @param    {[type]}   url    请求的 url
+ * @param    {[type]}   data   请求传递的参数
+ * @param    {[type]}   config 请求额外的配置信息
+ */
+function DELETE (url, config) {
+  return new Promise(function(resolve, reject) {
+    axios.delete(url, config).then(function(res) {
+      resolve(res.data);
+    }).catch(function(err) {
+      reject(err);
+    });
+  });
+}
+
+export default {
+  GET,
+  POST,
+  PUT,
+  PATCH,
+  DELETE
+};
